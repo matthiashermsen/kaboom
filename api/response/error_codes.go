432b82c5e6d0ecd35f631f4307b7088ed0a64072
matthiashermsen <matthiashermsen@gmail.com>
2023-08-26 07:50:47 +0200

    feat: add custom not found handler

--- api/response/error_codes.go
+++ api/response/error_codes.go
@@ -3,4 +3,5 @@ package response
 const (
 	InternalError         string = "INTERNAL_ERROR"
 	AppVersionUnavailable string = "APP_VERSION_UNAVAILABLE"
+	NotFound              string = "NOT_FOUND"
 )
---
+++ api/route/not_found_test.go
@@ -0,0 +1,33 @@
+package route
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRespondWithNotFound(suite *testing.T) {
+	suite.Run("Respond with not found", func(testing *testing.T) {
+		logger := slog.New(slog.Default().Handler())
+
+		request, err := http.NewRequest("GET", "/made-up", nil)
+
+		assert.NoError(testing, err, "Expected no error when constructing request")
+
+		responseRecorder := httptest.NewRecorder()
+		handler := RespondWithNotFound(logger)
+
+		handler(responseRecorder, request)
+
+		assert.Equal(testing, http.StatusNotFound, responseRecorder.Code, fmt.Sprintf("Expected status code %d but got %d", http.StatusNotFound, responseRecorder.Code))
+
+		expectedResponseBody := `{"status":"failure","data":null,"error":{"code":"NOT_FOUND","message":"Could not find '/made-up'"}}`
+		actualResponseBody := responseRecorder.Body.String()
+
+		assert.JSONEq(testing, expectedResponseBody, actualResponseBody, fmt.Sprintf("Expected response body '%s', but got '%s'", expectedResponseBody, actualResponseBody))
+	})
+}
